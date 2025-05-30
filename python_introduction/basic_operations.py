2bb9924613760b9926d5387e53fe2802865ca1d5
Kelly Grace <111120964+Holozot@users.noreply.github.com>
2025-05-30 16:59:47 +0300

    Create  basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+#operations
+added = number1 + number2
+diff = number1 - number2
+product= number1 * number2
+
+# Output results
+print(f"Sum of {number1} and {number2} is {added}")
+print(f"Difference of {number1} and {number2} is {diff}")
+print(f"Product of {number1} and {number2} is {product}")
